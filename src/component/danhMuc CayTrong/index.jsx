fd4eff112b9c6fdb9326631febbbebcdc2104971
Jasmine <huyenpt@omegatheme.com>
2023-05-09 18:08:24 +0700

    update

--- src/component/danhMuc CayTrong/index.jsx
+++ src/component/danhMuc CayTrong/index.jsx
@@ -1,5 +1,36 @@
+import { Col, Input, Row, Select } from "antd";
 import React from "react";
 
 export default function DanhMucCayTrong() {
-  return <div>DanhMucCayTrong 3333</div>;
+  return (
+    <div>
+      <h2>Danh mục cây trồng</h2>
+      <Row gutter={120}>
+        <Col span={6}>
+          <div>Nhóm cây</div>
+          <Select
+            style={{ width: "100%" }}
+            options={[
+              { label: "Cây Dâu Tây", value: "dautay" },
+              { label: "Cây Bơ", value: "caybo" },
+              { label: "Cà Phê", value: "caphe" },
+              { label: "Hoa Cúc", value: "hoacuc" },
+              { label: "Hoa Dướng Dương", value: "huongduong" },
+              { label: "Bông Đồng Tiền", value: "dongtien" },
+              { label: "Hoa Hồng", value: "hoahong" },
+              { label: "Cây Chuối", value: "caychuoi" },
+            ]}
+          />
+        </Col>
+        <Col span={6}>
+          <div>Nhóm cây</div>
+          <Input />
+        </Col>
+        <Col span={6}>
+          <div>Nhóm cây</div>
+          <Input />
+        </Col>
+      </Row>
+    </div>
+  );
 }
